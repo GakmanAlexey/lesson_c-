754999194b6cc7d671d5f8eaeb18b5faac7a2a37
GakmanAlexey <gakman@ya.ru>
2022-08-23 01:45:11 +0300

    123

--- Lesson/Lesson/ls1/main.cpp
+++ Lesson/Lesson/ls1/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <clocale>
+#include <WIndows.h>
 
+class Runtime {
+public:
+	int x = 0;
+	int y = 0;
+	int count(int &x) {
+		x++;
+		return 0;
+	}
+
+};
 int main(int argc, const char* argv[]) {
-
-	std::cout << "Hi! \n";
-	return 0;
+	Runtime med;
+	std::cin >> med.x;
+	med.x++;
+	med.count(med.x);
+	std::cout << med.x;
 }
---
+++ Lesson/Lesson/ls2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(int argc, const char* argv[]) {
+
+	std::cout << "Hi! \n";
+
+	system("pause");
+
+	int num = 10;
+	int num2 = 20;
+	int result;
+	result = num + num2;
+	result++;
+	std::cout << "result = " << result;
+	return 0;
+}
